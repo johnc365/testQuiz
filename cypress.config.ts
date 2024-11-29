7f7ad717f9c0efbdb5d60fd8fc83f9d9d72d74ec
johnc365 <johnrcaldwell123@gmail.com>
2024-11-29 14:56:41 -0600

    set up cypress directories and files to prep for creating tests

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+    specPattern: "cypress/component/**/*.cy.{js,jsx,ts,tsx}",
+  },
+  e2e: {
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    baseUrl: "http://localhost:3001", // subject to change
+    supportFile: false,
+  },
+});
